71370782f9fb25a9a130e5bee8f7d97f9d624335
Matthew Shirazi <smatthew120@gmail.com>
2018-05-28 21:20:37 -0400

    Initial commit

---
+++ OuncesToGrams.cpp
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    
+    // VARIABLES
+    int start = 1;
+    double end = 15;        
+    
+    // OUTPUT TABLE OF OUNCES AND GRAMS  
+    std::cout << "Ounces" << setw(12) << "Grams" << "\n";
+    
+    for ( double ounces = start ; ounces <= end  ; ounces++ ) {
+        double grams = 28.35 * ounces ; 
+        std::abs(grams);
+        std::setprecision(2);
+        std::cout << setw(3) << ounces << setw(15) << grams << "\n";
+    }
+}
+
---
+++ build/Debug/Cygwin_1-Windows/OuncesToGrams.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin_1-Windows/OuncesToGrams.o: OuncesToGrams.cpp
